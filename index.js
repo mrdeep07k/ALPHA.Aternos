ba9e0706a7351a123a57cc8be51736f3c10cfea8
mrdeep07k <mrdeep07k@gmail.com>
2026-08-11 22:13:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1945,7 +1945,7 @@ function sendDiscordWebhook(content, color = 0x0099ff) {
         description: content,
         color: color,
         timestamp: new Date().toISOString(),
-        footer: { text: "Slobos AFK Bot" },
+        footer: { text: "ALPHA-NIXUS" },
       },
     ],
   });
